54a4f322e323b7bfe98cda868c66ab537b3d1592
StanleyDip <stanleydr77@gmail.com>
2023-02-28 11:16:36 +0600

    expenseItem made dynamic

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
 import './ExpenseItem.css';
-function ExpenseItem() {
+function ExpenseItem(props) {
+    
+
+
+
   return (
     <div className="expense-item">
-      <div>February 28th 2023</div>
+      <div>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
-      <h2>Car Insurance</h2>
-      <div className='expense-item__price'>$294.67</div>
+      <h2>{props.title}</h2>
+      <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
